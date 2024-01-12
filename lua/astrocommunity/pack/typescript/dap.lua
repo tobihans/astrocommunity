c4759abe44a923024f485748ec9b567c5f1d033a
Uzair Aftab <uzaaft@outlook.com>
2024-01-12 11:31:37 +0100

    feat(typescript): mvp dap support for ts

--- lua/astrocommunity/pack/typescript/dap.lua
+++ lua/astrocommunity/pack/typescript/dap.lua
@@ -1,8 +1,37 @@
 local utils = require "astrocore"
+local js_based_languages = {
+  "typescript",
+  "javascript",
+  "typescriptreact",
+  "javascriptreact",
+}
 
 return {
   "mfussenegger/nvim-dap",
   optional = true,
+  depepndencies = {
+    {
+      "microsoft/vscode-js-debug",
+      -- After install, build it and rename the dist directory to out
+      build = "npm install --legacy-peer-deps --no-save && npx gulp vsDebugServerBundle && rm -rf out && mv dist out",
+      version = "1.*",
+    },
+    {
+      "mxsdev/nvim-dap-vscode-js",
+      opts = {
+        debugger_path = vim.fn.resolve(vim.fn.stdpath "data" .. "/lazy/vscode-js-debug"),
+        adapters = {
+          "chrome",
+          "pwa-node",
+          "pwa-chrome",
+          "pwa-msedge",
+          "pwa-extensionHost",
+          "node-terminal",
+        },
+      },
+    },
+    "mxsdev/nvim-dap-vscode-js",
+  },
   config = function()
     local dap = require "dap"
     dap.adapters["pwa-node"] = {
@@ -18,27 +47,59 @@ return {
         },
       },
     }
-    local js_config = {
+    local dap_config = {
+      -- Debug single nodejs files
       {
         type = "pwa-node",
         request = "launch",
         name = "Launch file",
         program = "${file}",
-        cwd = "${workspaceFolder}",
+        cwd = vim.fn.getcwd(),
+        sourceMaps = true,
       },
+      -- Debug nodejs processes (make sure to add --inspect when you run the process)
       {
         type = "pwa-node",
         request = "attach",
         name = "Attach",
         processId = require("dap.utils").pick_process,
-        cwd = "${workspaceFolder}",
+        cwd = vim.fn.getcwd(),
+        sourceMaps = true,
+      },
+      -- Debug web applications (client side)
+      {
+        type = "pwa-chrome",
+        request = "launch",
+        name = "Launch & Debug Chrome",
+        url = function()
+          local co = coroutine.running()
+          return coroutine.create(function()
+            vim.ui.input({
+              prompt = "Enter URL: ",
+              default = "http://localhost:3000",
+            }, function(url)
+              if url == nil or url == "" then
+                return
+              else
+                coroutine.resume(co, url)
+              end
+            end)
+          end)
+        end,
+        webRoot = vim.fn.getcwd(),
+        protocol = "inspector",
+        sourceMaps = true,
+        userDataDir = false,
       },
     }
 
-    if not dap.configurations.javascript then
-      dap.configurations.javascript = js_config
-    else
-      utils.extend_tbl(dap.configurations.javascript, js_config)
+    for _, language in ipairs(js_based_languages) do
+      if not dap.configurations[language] then
+        dap.configurations[language] = dap_config
+      else
+        dap.configurations[language] = dap_config
+        utils.extend_tbl(dap.configurations[language], dap_config)
+      end
     end
   end,
 }
